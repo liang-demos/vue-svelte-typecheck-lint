1b7cc5f6acb2c82be959db24b20c6d2546dab17a
Liang Mi <github@liangmi.dev>
2026-05-02 11:26:05 +0800

    chore

--- svelte/source.svelte
+++ svelte/source.svelte
@@ -1,11 +1,8 @@
-<script lang="ts">
-  import { writable } from 'svelte/store'
-
-  export let label: string
-
-  const count = writable(0)
-
-  $: doubled = $count * 2
+<script>
+  export let prop;
+  let count = 0;
 </script>
 
-<p>{label}: {doubled}</p>
+<button>
+  {count}
+</button>
--- vue/source.vue
+++ vue/source.vue
@@ -1,7 +1,4 @@
 <script setup lang="ts">
-import { ref } from 'vue'
-
-const b = ref(1)
 </script>
 
 <template>
